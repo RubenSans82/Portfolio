0d311d596867f548848717d9af581fd0b682acc3
RubenSans82 <rubensans82@gmail.com>
2025-07-08 12:59:25 +0200

    herramientas

--- chat-backend/simple-test.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Test básico para InfinityFree
-header('Content-Type: text/plain');
-echo "PHP OK - " . date('Y-m-d H:i:s') . "\n";
-echo "Server: " . $_SERVER['SERVER_NAME'] . "\n";
-echo "PHP Version: " . phpversion() . "\n";
-echo "Request Method: " . $_SERVER['REQUEST_METHOD'] . "\n";
-?>
